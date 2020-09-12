65f0d2ecf2173f9e02ca8ce4644c6e7c1979e6bb
nitzanbener <nitzanbenner@gmail.com>
2020-09-12 19:29:12 +0300

    Update user fields to support new auth strategy

--- models/User.js
+++ models/User.js
@@ -3,13 +3,20 @@ import mongoose from 'mongoose';
 import { roles } from '../enums/Users';
 
 const UserSchema = new mongoose.Schema({
+    username: { type: String },
+    password: { type: String },
     name: {
         first: { type: String },
         last: { type: String },
     },
-    username: { type: String },
-    password: { type: String },
-    email: { type: String },
+    email: {
+        isVerified: { type: Boolean },
+        address: { type: String },
+    },
+    phone: {
+        isVerified: { type: Boolean },
+        number: { type: String },
+    },
     role: { type: String, enum: roles }
 });
 
