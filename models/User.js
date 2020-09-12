161c60a49cd28c19ba6b20b09b6a1069c4923d23
nitzanbener <nitzanbenner@gmail.com>
2020-09-12 16:21:34 +0300

    Add fields to support new authentication strategy and user roles

--- models/User.js
+++ models/User.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose';
 
+import { roles } from '../enums/Users';
+
 const UserSchema = new mongoose.Schema({
     name: {
         first: { type: String },
         last: { type: String },
     },
+    username: { type: String },
+    password: { type: String },
     email: { type: String },
+    role: { type: String, enum: roles }
 });
 
 export default mongoose.model('User', UserSchema);
