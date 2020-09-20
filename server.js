6bd1444b0deb8898399e3a9246362a0bd3d56ab0
nitzanbener <nitzanbenner@gmail.com>
2020-09-20 14:11:06 +0300

    Replace jwt auth packages for easier use. Change start command to load dotenv so we could use 'imports' and not be afraid of them being hoisted

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import passport from 'passport';
+
+import './config/passport';
 
 import ClimbingRoutes from './routes/ClimbingRoutes';
 import User from './routes/User';
 import Crags from './routes/Crags';
 import Sectors from './routes/Sectors';
 
-dotenv.config();
-
-// We use require here as imports are hoisted and thus happen before "dotenv.config()",
-//  making the process.env vars invisible for the file
-require('./config/passport');
-
 // Constants
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // App
 const app = express();
@@ -33,7 +28,7 @@ mongoose.connect(process.env.MONGO_CONNECTION, {
 mongoose.Promise = global.Promise; // Use native promises as mongoose promises
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 app.use('/climbingroutes', ClimbingRoutes);
