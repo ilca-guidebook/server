afcadc8c6ee1fccd61bbeaa99dbfe1f6680973a6
nitzanbener <nitzanbenner@gmail.com>
2020-12-04 15:31:28 +0200

    PR changes

--- helpers/user/index.js
+++ helpers/user/index.js
@@ -2,5 +2,5 @@ const CODE_LENGTH = 900000 - 1; // 6 DIGIT NUMBER BETWEEN 0 TO 900000
 const ADD_TO_CODE = 100000;
 
 export const generateAuthCode = () => {
-    return `${Math.floor(ADD_TO_CODE + Math.random() * CODE_LENGTH)}`;
+    return Math.floor(Math.random() * CODE_LENGTH) + ADD_TO_CODE;;
 };
