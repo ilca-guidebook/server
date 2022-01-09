7e9399f5bcc725739f385d65c6fc98355339bf4e
nitzanbener <nitzanbenner@gmail.com>
2022-01-09 17:54:13 +0200

    Remove unused files

--- routes/user.js
+++ routes/user.js
@@ -2,56 +2,8 @@ import express from 'express';
 
 import UserModel from '../models/User';
 import { isUserPartOfILCA } from '../apis/loglig';
-import { encryptIdNumber } from '../utils/encryption';
 
 const router = express.Router();
-
-// router.post('/register', async (req, res) => {
-//     const {
-//         body: { email, firstName, lastName, phone },
-//     } = req;
-
-//     const existingUser = await UserModel.findOne({
-//         'email.address': email,
-//     }).exec();
-//     if (existingUser) {
-//         return res.sendStatus(409);
-//     }
-
-//     if (!email) {
-//         return res.status(422).json({
-//             errors: { email: 'is required' },
-//         });
-//     }
-
-//     const user = new UserModel({
-//         email: { address: email },
-//         phone: { number: phone },
-//         name: { first: firstName, last: lastName },
-//     });
-
-//     await user.save();
-
-//     return res.json({ user: user.toJSON() });
-// });
-
-/**
- * -router.get('/validateAuthCode', async (req, res) => {
--    const {
--        query: { email, code },
--    } = req;
--
--    const user = await UserModel.findOne({ 'email.address': email }).exec();
--    const codeValidated = await user.validateAuthCode(code);
--
--    if (!codeValidated) {
--        return res.sendStatus(401);
--    }
--
--    res.append('token', user.generateJWT());
--    return res.json({ user: user.toJSON() });
--});
- */
 
 router.post('/login', async (req, res) => {
     const {
