0ec01092698faac9dcd2b4dadddb446c7a434c42
nitzanbener <nitzanbenner@gmail.com>
2022-01-25 17:04:20 +0200

    Add images

--- routes/contentful.js
+++ routes/contentful.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { extractCragData } from '../utils/contentful';
 import contentfulClient from '../config/conentful';
 
 const router = express.Router();
@@ -10,49 +11,7 @@ router.get('/', async (req, res) => {
         include: 2,
     });
 
-    const crags = entries.items.map(({ fields, sys: { id } }) => {
-        const cragFeatures = {
-            rockType: fields.rockType,
-            routesLength: fields.routesLength,
-            season: fields.season,
-            shade: fields.shade,
-            cellularCoverage: fields.cellularCoverage,
-        };
-
-        return {
-            id,
-            ...fields,
-            cragFeatures,
-            sectors: fields.sectors.map(({ fields: sectorFields, sys: { id } }) => {
-                const climbingRoutes = (sectorFields.climbingRoute || []).map(({
-                    fields: climbingRouteFields,
-                    sys: { id },
-                }) => ({ ...climbingRouteFields, id }));
-                const boulderProblems = (sectorFields.boulderProblem || []).map(({
-                    fields: boulderProblemsFields,
-                    sys: { id },
-                }) => ({ ...boulderProblemsFields, id }));
-
-                const sectorData = {
-                    ...sectorFields,
-                    id,
-                    climbingRoutes,
-                    boulderProblems,
-                };
-
-                const { image: { fields: { file: { url: sectorImage } = {} } = {} } = {} } = sectorFields;
-
-                if (sectorImage) {
-                    sectorData.sectorImage = `https:${sectorImage}`;
-                }
-
-                delete sectorData.climbingRoute;
-                delete sectorData.boulderProblem;
-
-                return sectorData;
-            }),
-        };
-    });
+    const crags = entries.items.map(extractCragData);
 
     return res.json(crags);
 });
---
+++ utils/contentful.js
@@ -0,0 +1,71 @@
+const extractSectorsData = (sectors) => {
+    const { fields: sectorFields, sys: { id } } = sectors;
+    
+    const climbingRoutes = (sectorFields.climbingRoute || []).map(({
+        fields: climbingRouteFields,
+        sys: { id },
+    }) => ({ ...climbingRouteFields, id }));
+    const boulderProblems = (sectorFields.boulderProblem || []).map(({
+        fields: boulderProblemsFields,
+        sys: { id },
+    }) => ({ ...boulderProblemsFields, id }));
+
+    const sectorData = {
+        ...sectorFields,
+        id,
+        climbingRoutes,
+        boulderProblems,
+    };
+
+    const { image: { fields: { file: { url: sectorImage } = {} } = {} } = {} } = sectorFields;
+
+    if (sectorImage) {
+        sectorData.sectorImage = `https:${sectorImage}`;
+    }
+
+    delete sectorData.climbingRoute;
+    delete sectorData.boulderProblem;
+
+    return sectorData;
+};
+
+export const extractCragData = (crags) => {
+    const { fields, sys: { id } } = crags;
+
+    const cragFeatures = {
+        rockType: fields.rockType,
+        routesLength: fields.routesLength,
+        season: fields.season,
+        shade: fields.shade,
+        cellularCoverage: fields.cellularCoverage,
+    };
+
+    const cragData = {
+        id,
+        ...fields,
+        cragFeatures,
+        sectors: fields.sectors.map(extractSectorsData),
+    };
+
+    const {
+        coverImage: { fields: { file: { url: coverImageUrl } = {} } = {} } = {},
+        galleryImages,
+    } = fields;
+
+    if (coverImageUrl) {
+        cragData.coverImage = coverImageUrl;
+    }
+
+    if (galleryImages && galleryImages.length) {
+        cragData.galleryImages = galleryImages.map((image) => {
+            const { fields: { file: { url } = {} } = {} } = image;
+
+            return {
+                fullSize: `https:${url}`,
+                thumbnail: `https:${url}?w=200&h=200&fit=crop`,
+            };
+        });
+    }
+
+    return cragData;
+};
