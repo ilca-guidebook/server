6ef345cff33cea70e476be7acf22a101a6e6b1e9
nitzanbener <nitzanbenner@gmail.com>
2024-06-24 22:59:06 +0300

    Add timestamps

--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
   phoneNumber: { type: String },
   idNumber: { type: String },
   pushToken: { type: String },
-});
+}, { timestamps: true });
 
 UserSchema.methods.generateJWT = function () {
   const today = new Date();
