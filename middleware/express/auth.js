5e3be91c0593441a94842521e074761e6caf3e56
Nitzan Ben Ner <nitzanbenner@gmail.com>
2026-06-05 18:30:47 +0300

    Remove logs

--- middleware/express/auth.js
+++ middleware/express/auth.js
@@ -5,8 +5,6 @@ const getTokenFromHeaders = (req) => {
     headers: { authorization },
   } = req;
 
-  console.log('nitzan authorization', authorization);
-
   if (authorization && authorization.split(' ')[0] === 'Bearer') {
     return authorization.split(' ')[1];
   }
